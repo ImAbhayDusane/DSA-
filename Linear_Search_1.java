ab3348fda6789316656f062b8e54618df401ce8d
ImAbhayDusane <98648808+ImAbhayDusane@users.noreply.github.com>
2022-02-01 12:04:33 +0530

    Create Linear_Search_1.java

---
+++ Linear_Search_1.java
@@ -0,0 +1,25 @@
+// Linear Search 2 here we will return element rather than writing index value
+public interface Linear_Search {
+    public static void main(String[] args) {
+        // the actual question
+        int[] array = {21, 23, 3, 2, 11, 90, 18};
+        int targetElement = 90;
+        int result = linearSearch(array, targetElement);
+        System.out.println(result);
+    }
+    static int linearSearch(int[] arr, int targetElement){
+        if(arr.length == 0){
+            return -1;
+        }
+        // if we target element found then just return the element value
+        for (int index = 0; index < arr.length; index++) {
+            int element = arr[index];
+            if(element == targetElement){
+                return element;
+            }
+        }
+        // if we are unable to get any result from above process it will print -1
+        return -1;
+    }
+
+}
