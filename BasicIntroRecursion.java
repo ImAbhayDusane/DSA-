ae75696da8803002e3ace6c734d7ec9ea9df5896
ImAbhayDusane <98648808+ImAbhayDusane@users.noreply.github.com>
2022-02-03 08:47:24 +0530

    Create BasicIntroRecursion.java

---
+++ BasicIntroRecursion.java
@@ -0,0 +1,10 @@
+public class BasicIntroRecursion {
+    public static void main(String[] args) {
+        // Write a function that give some important message
+        msg();
+    }
+    static void msg(){
+        System.out.println("Without Recursion you cannot learn DSA");
+    }
+
+}
